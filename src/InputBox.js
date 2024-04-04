e691acb736f10c2b31b3290dca824de19b9b11dc
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2024-04-04 00:33:40 -0400

    refactor out interraction

--- src/InputBox.js
+++ src/InputBox.js
@@ -1,44 +1,8 @@
 import { SendHorizontal } from 'lucide-react';
 import React, { useState } from 'react';
 import './InputBox.css'; // Import CSS file for styling
-// import { VFInteract } from './VoiceflowInteractions'
+import { VFInteract } from './VoiceflowInteractions'
 
-const API_KEY = 'VF.DM.660dc8d6df5b4249cccd010b.tRWxpTLoHqZZ2HNp';
-
-// const RUNTIME_API_URL = 'https://general-runtime.voiceflow.com/' // change this to whatever your API URL is
-
-const VFInteract = async (user, message) => {
-    const interractionUrl = `https://general-runtime.voiceflow.com/state/user/newer/interact`;
-    console.log(interractionUrl);
-    console.log(message);
-
-    const payload = {
-        "action": {
-            "type": "text",
-            "payload": message
-        }
-    };
-
-    console.log(payload);
-
-    let data = await fetch(interractionUrl, {
-        headers: {
-            Authorization: API_KEY, accept: "application/json", "content-type": "application/json",
-        },
-        method: "POST",
-        body: JSON.stringify(payload),
-    })
-    let postRes = await data.json();
-    console.log(postRes);
-    let textResponses = [];
-    for (let i = 0; i < postRes.length; i++) {
-        if (postRes[i].type === "text") {
-            textResponses.push(postRes[i].payload.message);
-        }
-    }
-    console.log(textResponses);
-    return textResponses;
-}
 
 const InputBox = ({ addMessage }) => {
     const [inputValue, setInputValue] = useState('');
--- src/VoiceflowInteractions.js
+++ src/VoiceflowInteractions.js
@@ -1,1 +1,34 @@
+const API_KEY = 'VF.DM.660dc8d6df5b4249cccd010b.tRWxpTLoHqZZ2HNp';
 
+const RUNTIME_API_URL = 'https://general-runtime.voiceflow.com/'
+
+const VFInteract = async (user, message) => {
+    const interractionUrl = `https://general-runtime.voiceflow.com/state/user/newester/interact`;
+    console.log(interractionUrl);
+    console.log(message);
+
+    const payload = {
+        "action": {
+            "type": "text",
+            "payload": message
+        }
+    };
+
+    let data = await fetch(interractionUrl, {
+        headers: {
+            Authorization: API_KEY, accept: "application/json", "content-type": "application/json", 'versionID': 'production'
+        },
+        method: "POST",
+        body: JSON.stringify(payload),
+    })
+    let postRes = await data.json();
+    let textResponses = [];
+    for (let i = 0; i < postRes.length; i++) {
+        if (postRes[i].type === "text") {
+            textResponses.push(postRes[i].payload.message);
+        }
+    }
+    return textResponses;
+}
+
+export { VFInteract };
