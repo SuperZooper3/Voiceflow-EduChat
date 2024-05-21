67d0a36a8a598bf539f744899114eabef71c2b55
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2024-05-21 17:59:51 -0400

    header css

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './App.css';
+import './Header.css';
 import articleList from './articleList.json';
 
 const Header = ({handleArticleSelect}) => {
   return (
     <div className='app-header'>
-      <h1>EduChat</h1>
-      {articleList.map((article) => (
-        <button
-          key={article.id}
-          onClick={() => handleArticleSelect(article.id)}
-        >
-          {article.displayname}
-        </button>
-      ))}
+      <h1 className='header-title'>EduChat</h1>
+      <div className='article-select-wrapper'>
+        {articleList.map((article) => (
+          <button
+            className='article-select-button'
+            key={article.id}
+            onClick={() => handleArticleSelect(article.id)}
+          >
+            {article.displayname}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
